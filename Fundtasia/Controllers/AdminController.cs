6aea632f5cc85f0debc98000d75245414588a40a
WILLIAM CHOONG <67597772+CWilliam001@users.noreply.github.com>
2021-11-16 19:52:19 +0800

    Modify Admin Controller and Admin layout

--- Fundtasia/Controllers/AdminController.cs
+++ Fundtasia/Controllers/AdminController.cs
@@ -13,5 +13,37 @@ namespace Fundtasia.Controllers
         {
             return View();
         }
+
+        public ActionResult Staff()
+        {
+            return View();
+        }
+
+        //Actually this is the user page, but because of we use the "User" word to become the method it will have the conflict with the Controller.User
+        public ActionResult ClientUser()
+        {
+            return View();
+        }
+
+        public ActionResult Event()
+        {
+            return View();
+        }
+
+        public ActionResult Merchandise()
+        {
+            return View();
+        }
+
+        public ActionResult Donation()
+        {
+            return View();
+        }
+
+        public ActionResult Report()
+        {
+            return View();
+        }
+
     }
 }
