15a7fc29b275fd3fe4c0fe9fdb1ecd24293a333b
Yoke Koon Soh <sohyk-pm19@student.tarc.edu.my>
2021-11-23 20:12:33 +0800

    Add ClientUser & Staff layout

--- Fundtasia/Controllers/AdminController.cs
+++ Fundtasia/Controllers/AdminController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Fundtasia.Models;
 
 namespace Fundtasia.Controllers
 {
     public class AdminController : Controller
     {
+        DBEntities1 db = new DBEntities1();
+
         // GET: Admin
         public ActionResult Dashboard()
         {
@@ -16,13 +19,15 @@ namespace Fundtasia.Controllers
 
         public ActionResult Staff()
         {
-            return View();
+            var model = db.Users;
+            return View(model);
         }
 
         //Actually this is the user page, but because of we use the "User" word to become the method it will have the conflict with the Controller.User
         public ActionResult ClientUser()
         {
-            return View();
+            var model = db.Users;
+            return View(model);
         }
 
         public ActionResult Event()
