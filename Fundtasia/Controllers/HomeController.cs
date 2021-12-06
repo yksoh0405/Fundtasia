b3015765ec6f0a8d678d74d7d06ac1463e71bf5e
hey awak <70712101+FlavouredSocks@users.noreply.github.com>
2021-12-07 03:55:32 +0800

    Finish DonationPayment, Add and Modify DonationReceipt

--- Fundtasia/Controllers/HomeController.cs
+++ Fundtasia/Controllers/HomeController.cs
@@ -37,6 +37,20 @@ namespace Fundtasia.Controllers
 
         public ActionResult DonationPayment()
         {
+            if (!Request.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            if (Session["UserSession"] != null)
+            {
+                User loginUser = (User)Session["UserSession"];
+                if (String.Equals(loginUser.Role, "User"))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
             ViewBag.EventList = new SelectList(db.Events, "Id", "Title");
             return View();
         }
@@ -44,22 +58,93 @@ namespace Fundtasia.Controllers
         [HttpPost]
         public ActionResult DonationPayment(Donation model)
         {
+            if (!Request.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            if (Session["UserSession"] != null)
+            {
+                User loginUser = (User)Session["UserSession"];
+                model.UserId = loginUser.Id;
+                if (String.Equals(loginUser.Role, "User"))
+                {
+                    return RedirectToAction("Index", "Home");
+                } 
+            }
+
             if (ModelState.IsValid)
             {
-                model.TimeDonated = DateTime.Now;
-                db.Donations.Add(model);
+                model.Id = Guid.NewGuid();
+
+                var d = new Donation
+                {
+                    Id = model.Id,
+                    UserId = model.UserId,
+                    TimeDonated = DateTime.Now,
+                    Amount = model.Amount,
+                    EventId = model.EventId
+                };
+
+                db.Donations.Add(d);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("DonationReceipt", "Home");
             }
 
             ViewBag.EventList = new SelectList(db.Donations, "Id", "Title");
             return View(model);
         }
 
-        public ActionResult DonationReceipt()
+        public ActionResult DonationReceipt(string sort = "Time Donated", string sortdir = "DESC", int page = 1, string keyword = "")
         {
-            //pass img, price and name from donation payment
-            return View();
+            if (!Request.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            if (Session["UserSession"] != null)
+            {
+                User loginUser = (User)Session["UserSession"];
+                if (String.Equals(loginUser.Role, "User"))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            Func<Donation, object> fn = d => d.Id;
+            switch (sort)
+            {
+                case "Id": fn = d => d.Id; break;
+                case "Donor": fn = d => d.User.FirstName; break;
+                case "Time Donated": fn = d => d.TimeDonated; break;
+                case "Amount": fn = d => d.Amount; break;
+                case "Event": fn = d => d.EventId; break;
+            }
+
+            var sorted = sortdir == "DESC" ? db.Donations.Where(s => s.User.FirstName.Contains(keyword)).OrderByDescending(fn) : db.Donations.Where(s => s.User.FirstName.Contains(keyword)).OrderBy(fn);
+
+            //Paging
+            if (page < 1)
+            {
+                return RedirectToAction(null, new { page = 1 });
+            }
+
+            var model = sorted.ToPagedList(page, 10);
+
+            if (model == null)
+            {
+                return View();
+            }
+
+            if (page > model.PageCount)
+            {
+                return RedirectToAction(null, new { page = model.PageCount });
+            }
+
+            //Ajax Request
+            if (Request.IsAjaxRequest()) return PartialView("_DonationReceipt", model);
+
+            return View(model);
         }
 
         public ActionResult MerchandisePayment(string id)
---
+++ Fundtasia/Models/Extended/Payment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fundtasia.Models
+{
+    [MetadataType(typeof(PaymentMetadata))]
+    public partial class Payment
+    {
+
+    }
+
+    public class PaymentMetadata
+    {
+        [Display(Name = "Name on card")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name on card is required")]
+        public string Name { get; set; }
+
+        [Display(Name = "Card Number")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Card Number is required")]
+        public string CardNumber { get; set; }
+
+        [Display(Name = "Expiry Date")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Expiry Date is required")]
+        public DateTime ExpiryDate { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "CVV is required")]
+        [RegularExpression(@"\d{3}", ErrorMessage = "Invalid CVV format.")]
+        public string CVV { get; set; }
+
+    }
+
+    public class DonationPaymentVM
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "*Donation Amount is required")]
+        public decimal Amount { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "*Event Selection is required")]
+        public string EventId { get; set; }
+    }
+}
