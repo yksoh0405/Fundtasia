7dd84a881231b1f2964a234ac2d1732046116bba
WILLIAM CHOONG <67597772+CWilliam001@users.noreply.github.com>
2021-12-08 21:16:20 +0800

    Modify merchandise payment(in progress)

--- Fundtasia/Models/Extended/Payment.cs
+++ Fundtasia/Models/Extended/Payment.cs
@@ -64,7 +64,6 @@ namespace Fundtasia.Models
         public string State { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "City is required")]
-        [RegularExpression(@"^[A-Za-z ]+$", ErrorMessage = "Invalid {0} format.")]
         public string City { get; set; }
 
         [Display(Name = "Postal Code")]
