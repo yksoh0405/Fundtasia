ca8d59564290f53c6fa9c44f92a653e2cc6de64a
WILLIAM CHOONG <67597772+CWilliam001@users.noreply.github.com>
2021-12-09 19:52:32 +0800

    Modify DB.edmx

--- Fundtasia/Models/User.cs
+++ Fundtasia/Models/User.cs
@@ -19,8 +19,8 @@ namespace Fundtasia.Models
         {
             this.Donations = new HashSet<Donation>();
             this.Events = new HashSet<Event>();
-            this.Reports = new HashSet<Report>();
             this.PasswordResets = new HashSet<PasswordReset>();
+            this.Reports = new HashSet<Report>();
             this.UserMerchandises = new HashSet<UserMerchandise>();
         }
     
@@ -41,9 +41,9 @@ namespace Fundtasia.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Event> Events { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Report> Reports { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<PasswordReset> PasswordResets { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Report> Reports { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<UserMerchandise> UserMerchandises { get; set; }
     }
